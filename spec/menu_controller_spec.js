d713f70f631732e623fb984ec8467356daaa0c36
jensung <jensunngggg@gmail.com>
2019-05-20 11:06:49 -0400

    Assignment 2 Testing: Completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,16 +1,21 @@
-const MenuController = require("../controllers/MenuController");
+const MenuController = require('../controllers/MenuController');
 
 describe("MenuController", () => {
   beforeEach(() => {
     this.menu = new MenuController();
   });
   describe("#getContactCount()", () => {
-    it("should return 0 when no contacts are in the book", () => {
+    it ("should return 0 when no contacts are in the book", () => {
       expect(this.menu.getContactCount()).toBe(0);
     });
-    it("should return 1 when there is exactly one contact in the book", () => {
+    it ("should return 1 when there is exactly one contact in the book", () => {
       this.menu.contacts.push("Bob");
-      expect(this.menu.getContactCount()).toBe(1)
+      expect(this.menu.getContactCount()).toBe(1);
+    });
+  });
+  describe("#remindMe()", () => {
+    it ("should return the string 'Learning is a life-long pursuit!'", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit!");
     });
   });
 });
